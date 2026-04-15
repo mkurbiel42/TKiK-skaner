cd1fbb3e7fc25c621f8c0643f273322524284490
unknown <michal.kurbiel42@gmail.com>
2026-04-15 21:11:08 +0200

    changed main

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from scanner.scanner import scan
 from scanner.scanner_language import scan as scan_l
 from scanner.syntax_colorizer import colorizeTokens
-from scanner.scan_response import ScanResponse, ScanResponseType
-from scanner.token import Token, LanguageToken
 from typing import List
 
 scType = input("Wybierz rodzaj skanera: 1 - wyrażenie matematyczne, 2 - własny język: ")
